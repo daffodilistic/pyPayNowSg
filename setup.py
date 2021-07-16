0b4794cc7f8ea86531383e238426e2a8117795db
Soh Thiam Hing <daffodilistic@gmail.com>
2021-07-16 13:24:38 +0800

    Update requirements for Python v3

--- setup.py
+++ setup.py
@@ -102,7 +102,11 @@ setup(
         # that you indicate you support Python 3. These classifiers are *not*
         # checked by 'pip install'. See instead 'python_requires' below.
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Operating System :: OS Independent'
     ],
 
